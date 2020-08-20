62cba5f0b3e88e4296a44829a7cc30d306b40883
Garret G <the-royal@github.com>
2020-08-19 19:21:58 -0500

    add sukari

--- docs/assets/js/keyboards/sukari/configuration.js
+++ docs/assets/js/keyboards/sukari/configuration.js
@@ -6,21 +6,20 @@ sukari.id = 'sukari_rev1';
 
 // config
 sukari.config = {
-    vendorId: '0xFEED',
-    productId: '0x62AC',
+    vendorId: '0x4b4b',
+    productId: '0x1001',
     deviceVersion: '0x0001',
     manufacturer: 'The-Royal',
     product: 'Sukari',
     description: 'A "True Gasket" Ortho',
-    matrixRowPins: ['D0', 'D1', 'D3', 'F5', 'B2'],
-    matrixColumnPins: ['F0', 'F7', 'F6', 'F4', 'F1', 'E6', 'D6', 'D2', 'B4', 'D7', 'B6', 'D5'],
+    matrixRowPins: ['B4', 'B5', 'B6', 'B7', 'B0'],
+    matrixColumnPins: ['B1', 'B2', 'C7', 'C6', 'C5', 'C4', 'D6', 'C2', 'D0', 'D1', 'D5', 'D2'],
     diodeDirection: 'COL2ROW',
     matrixHasGhost: false,
     backlightLevels: 0,
-    backlightPin: 'B5',
     usbMaxPowerConsumption: 100,
     debouncingDelay: 5,
-    tappingTerm: 175,
+    tappingTerm: 200,
     lockingSupportEnabled: true,
     lockingResyncEnabled: true,
     commandKeyCombination: 'keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))',
@@ -43,13 +42,13 @@ sukari.rules = {
     bootloaderSize: 4096,
     bootmagicEnabled: true,
     mousekeyEnabled: true,
-    extrakeyEnabled: true,
+    extrakeyEnabled: false,
     consoleEnabled: false,
-    commandEnabled: true,
+    commandEnabled: false,
     sleepLedEnabled: false,
     nkroEnabled: true,
-    backlightEnabled: true,
-    rgbLightEnabled: true
+    backlightEnabled: false,
+    rgbLightEnabled: false
 };
 
 // keymap
@@ -73,30 +72,16 @@ sukari.configKeymap.positions = [
 // zones
 sukari.zones = [
     {
-        label: 'Right Shift',
-        value: 0,
-        choices: [
-            {
-                code: 0,
-                name: 'Standard'
-            },
-            {
-                code: 1,
-                name: 'Alternate'
-            }
-        ]
-    },
-    {
         label: 'Bottom Row',
         value: 0,
         choices: [
             {
                 code: 0,
-                name: 'Full Space'
+                name: 'Big-Space'
             },
             {
                 code: 1,
-                name: 'Split Space'
+                name: 'Grid'
             }
         ]
     }
@@ -108,7 +93,7 @@ sukari.keySections = [
         row1: [
           {
             id: 'k00',
-            value: 'TAB',
+            value: 'ESC',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -116,7 +101,7 @@ sukari.keySections = [
           },
           {
             id: 'k01',
-            value: 'Q',
+            value: '1',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -124,7 +109,7 @@ sukari.keySections = [
           },
           {
             id: 'k02',
-            value: 'W',
+            value: '2',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -132,7 +117,7 @@ sukari.keySections = [
           },
           {
             id: 'k03',
-            value: 'E',
+            value: '3',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -140,7 +125,7 @@ sukari.keySections = [
           },
           {
             id: 'k04',
-            value: 'R',
+            value: '4',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -148,7 +133,7 @@ sukari.keySections = [
           },
           {
             id: 'k05',
-            value: 'T',
+            value: '5',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -156,7 +141,7 @@ sukari.keySections = [
           },
           {
             id: 'k06',
-            value: 'Y',
+            value: '6',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -164,7 +149,7 @@ sukari.keySections = [
           },
           {
             id: 'k07',
-            value: 'U',
+            value: '7',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -172,7 +157,7 @@ sukari.keySections = [
           },
           {
             id: 'k08',
-            value: 'I',
+            value: '8',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -180,7 +165,7 @@ sukari.keySections = [
           },
           {
             id: 'k09',
-            value: 'O',
+            value: '9',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -188,7 +173,7 @@ sukari.keySections = [
           },
           {
             id: 'k0A',
-            value: 'P',
+            value: '0',
             type: 'normal',
             secondary: null,
             showMenu: false,
@@ -196,14 +181,451 @@ sukari.keySections = [
           },
           {
             id: 'k0B',
+            value: 'MINUS',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          }
+        ],
+        row2: [
+          {
+            id: 'k10',
+            value: 'TAB',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k11',
+            value: 'Q',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k12',
+            value: 'W',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k13',
+            value: 'E',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k14',
+            value: 'R',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k15',
+            value: 'T',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k16',
+            value: 'Y',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k17',
+            value: 'U',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k18',
+            value: 'I',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k19',
+            value: 'O',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k1A',
+            value: 'P',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k1B',
             value: 'BSPC',
             type: 'normal',
             secondary: null,
             showMenu: false,
             shape: 'k100'
           }
+        ],
+        row3: [
+          {
+            id: 'k20',
+            value: 'LCTL',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k21',
+            value: 'A',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k22',
+            value: 'S',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k23',
+            value: 'D',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k24',
+            value: 'F',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k25',
+            value: 'G',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k26',
+            value: 'H',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k27',
+            value: 'J',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k28',
+            value: 'K',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k29',
+            value: 'L',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k2A',
+            value: 'SCOL',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k2B',
+            value: 'QUOTE',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          }
+        ],
+        row4: [
+          {
+            id: 'k30',
+            value: 'LSFT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k31',
+            value: 'Z',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k32',
+            value: 'X',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k33',
+            value: 'C',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k34',
+            value: 'V',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k35',
+            value: 'B',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k36',
+            value: 'N',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k37',
+            value: 'M',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k38',
+            value: 'COMMA',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k39',
+            value: 'DOT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k3A',
+            value: 'SLSH',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k3B',
+            value: 'ENTER',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          }
+        ],
+        row5: [
+          {
+            id: 'k40',
+            value: 'DEL',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k41',
+            value: 'LGUI',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k42',
+            value: 'MENU',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k43',
+            value: 'LALT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k44',
+            value: 'PGUP',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k45',
+            value: 'SPACE',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k46',
+            value: 'SPACE',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k47',
+            value: 'PGDN',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k48',
+            value: 'LEFT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k49',
+            value: 'DOWN',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k4A',
+            value: 'UP',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k4B',
+            value: 'RIGHT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          }
         ]
+        [
+          {
+            id: 'k40',
+            value: 'DEL',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          },
+          {
+            id: 'k41',
+            value: 'LALT',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k150'
+          },
+          {
+            id: 'k42',
+            value: 'SPACE',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k700'
+          },
+          {
+            id: 'k4A',
+            value: 'RGUI',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k150'
+          },
+          {
+            id: 'k4B',
+            value: 'RCTL',
+            type: 'normal',
+            secondary: null,
+            showMenu: false,
+            shape: 'k100'
+          }
+
+        ]
+
     }
+
 ];
 
 sukari.keymap = function() {
@@ -218,8 +640,9 @@ sukari.keymap = function() {
         layer = new Array();
         layer.push(ks.row1);
         layer.push(ks.row2);
-        layer.push(ks.row3.concat(ks.rightShift[zones[0].value]));
-        layer.push(ks.bottomRow[zones[1].value])
+        layer.push(ks.row3);
+        layer.push(ks.row4);
+        layer.push(ks.row5[zones[1].value])
         keymap.push(layer);
     }
 
